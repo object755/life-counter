3c2ec631f4a0d0a08008dbd02fdbe4f55cd085fc
object755 <bfgoodman756@gmail.com>
2023-11-05 21:08:13 +0200

    render function complete

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let age = document.querySelector(".age-number");
 
-age.addEventListener("change", renderNodes(e));
+let ageTotal = document.querySelector(".age_total");
+
+ageTotal.addEventListener("change", renderNodes);
 
 
 function renderNodes() {
-    
+    let weeksInYear = 52;
+    let nodesCount = ageTotal.value *  weeksInYear;
+
+    let lifeNodesContainer = document.querySelector(".life_nodes");
+
+    lifeNodesContainer.innerHTML = "";
+
+    for (let i = 0; i < nodesCount; ++i) {
+        let node = document.createElement('div');
+        node.className = 'life_node';
+
+        lifeNodesContainer.append(node);
+    }
 }
+
+renderNodes()
